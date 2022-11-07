11c41dff0d2f51cc3fb6469a38f3b76dd68f3beb
Emad Ali <mc.lilzee@hotmail.com>
2022-11-08 00:39:10 +0100

    Add throw response on invalid input

--- src/main/java/project/accountservice/AccountServiceController.java
+++ src/main/java/project/accountservice/AccountServiceController.java
@@ -1,13 +1,22 @@
 package project.accountservice;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 @RestController
 public class AccountServiceController {
 
     @PostMapping("/api/auth/signup")
     public User signUp(@RequestBody User user) {
+        if (!isValid(user)) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+        }
         return user;
+    }
+
+    private boolean isValid(User user) {
+        return false;
     }
 
     @GetMapping("/api/empl/payment")
