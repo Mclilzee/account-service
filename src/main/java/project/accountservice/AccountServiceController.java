2e83242d0e6751944e5aa7f24b0167fde17fa485
Emad Ali <mc.lilzee@hotmail.com>
2022-11-08 00:42:53 +0100

    Validate post input

--- src/main/java/project/accountservice/AccountServiceController.java
+++ src/main/java/project/accountservice/AccountServiceController.java
@@ -1,22 +1,16 @@
 package project.accountservice;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.web.server.ResponseStatusException;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class AccountServiceController {
 
     @PostMapping("/api/auth/signup")
     public User signUp(@RequestBody User user) {
-        if (!isValid(user)) {
-            throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
-        }
         return user;
-    }
-
-    private boolean isValid(User user) {
-        return false;
     }
 
     @GetMapping("/api/empl/payment")
--- src/main/java/project/accountservice/User.java
+++ src/main/java/project/accountservice/User.java
@@ -1,5 +1,8 @@
 package project.accountservice;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.server.ResponseStatusException;
+
 public class User {
 
     enum Role {
@@ -15,11 +18,19 @@ public class User {
     private Role role;
 
     public User(String name, String lastName, String email, String password) {
+        if (name.isBlank() || lastName.isBlank() || email.isBlank() || password.isBlank() || !isValidEmail(email)) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+        }
+
         this.name = name;
         this.lastName = lastName;
         this.email = email;
         this.password = password;
         this.role = Role.USER;
+    }
+
+    private boolean isValidEmail(String email) {
+        return email.endsWith("acme.com");
     }
 
     public String getName() {
