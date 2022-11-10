c915a5f352c587c676757e198dbe3a50272f7c8a
Emad Ali <mc.lilzee@hotmail.com>
2022-11-10 18:49:55 +0100

    Fix user role bug

--- src/main/java/project/accountservice/SecurityConfiguration.java
+++ src/main/java/project/accountservice/SecurityConfiguration.java
@@ -1,15 +1,12 @@
 package project.accountservice;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.jdbc.JdbcDaoImpl;
-import org.springframework.security.provisioning.JdbcUserDetailsManager;
-import org.springframework.security.provisioning.UserDetailsManager;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-
-import javax.sql.DataSource;
 
 @Configuration
 public class SecurityConfiguration {
@@ -19,7 +16,7 @@ public class SecurityConfiguration {
         http.authorizeRequests()
                 .mvcMatchers("/api/admin").hasRole("ADMINISTRATOR")
                 .mvcMatchers("/api/acct").hasRole("ACCOUNTANT")
-                .mvcMatchers("/api/empl/payment").hasAnyRole("USER, ACCOUNTANT")
+                .mvcMatchers("/api/empl/payment").hasAnyRole("USER", "ACCOUNTANT")
                 .mvcMatchers("/api/auth/changepass").hasAnyRole("USER", "ACCOUNTANT", "ADMINISTRATOR")
                 .mvcMatchers("/api/auth/signup").permitAll()
                 .and().httpBasic()
@@ -29,23 +26,11 @@ public class SecurityConfiguration {
         return http.build();
     }
 
-//    @Bean
-//    public DataSource dataSource() {
-//        return new EmbeddedDatabaseBuilder()
-//                .setType(EmbeddedDatabaseType.H2)
-//                .addScript(JdbcDaoImpl.DEFAULT_USER_SCHEMA_DDL_LOCATION)
-//                .build();
-//    }
-//
-//    @Bean
-//    public UserDetailsManager users(DataSource dataSource) {
-//        UserDetails user = User.withDefaultPasswordEncoder()
-//                .username("user")
-//                .password("password")
-//                .roles("USER")
-//                .build();
-//        JdbcUserDetailsManager users = new JdbcUserDetailsManager(dataSource);
-//        users.createUser(user);
-//        return users;
-//    }
+    @Bean
+    public DaoAuthenticationProvider authProvider(@Autowired BCryptPasswordEncoder encoder, @Autowired UserDetailsServiceImp userDetailsService) {
+        DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
+        authenticationProvider.setPasswordEncoder(encoder);
+        authenticationProvider.setUserDetailsService(userDetailsService);
+        return authenticationProvider;
+    }
 }
