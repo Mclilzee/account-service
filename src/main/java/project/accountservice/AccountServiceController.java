732853587f22595e1e962374736b22e78d1bc04d
Emad Ali <mc.lilzee@hotmail.com>
2022-11-10 17:17:06 +0100

    Add user adding and checking by email

--- src/main/java/project/accountservice/AccountServiceController.java
+++ src/main/java/project/accountservice/AccountServiceController.java
@@ -1,17 +1,22 @@
 package project.accountservice;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import javax.validation.Valid;
+import java.util.Optional;
 
 @RestController
 public class AccountServiceController {
 
+    @Autowired
+    UserRepositoryService userRepositoryService;
 
     @Autowired
     BCryptPasswordEncoder encoder;
@@ -19,7 +24,13 @@ public class AccountServiceController {
     @PostMapping("/api/auth/signup")
     public User signUp(@Valid @RequestBody User user) {
         user.setPassword(encoder.encode(user.getPassword()));
-        return user;
+        Optional<User> newUser = userRepositoryService.getUserByEmail(user.getEmail());
+        if (newUser.isPresent()) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "User exist!");
+        }
+
+        userRepositoryService.addUser(user);
+        return userRepositoryService.getUserByEmail(user.getEmail()).get();
     }
 
     @GetMapping("/api/empl/payment")
