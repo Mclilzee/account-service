9958b173142008d9cbf735caf514bd7183404834
Emad Ali <mc.lilzee@hotmail.com>
2022-11-11 05:59:43 +0100

    Add password managing class

---
+++ src/main/java/project/accountservice/util/Password.java
@@ -0,0 +1,24 @@
+package project.accountservice.util;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+public class Password {
+
+    @NotBlank
+    @Size(min = 12, message = "The password length must be at least 12 chars!")
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    private String new_password;
+
+    public Password(String new_password) {
+        this.new_password = new_password;
+    }
+
+    public Password() {}
+
+    public String getPassword() {
+        return new_password;
+    }
+}
