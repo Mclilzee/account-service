d0727ffc191e54a689ecb825883ce877908a2469
Emad Ali <mc.lilzee@hotmail.com>
2022-11-11 03:50:45 +0100

    Create custom error message format

--- src/main/java/project/accountservice/exception/CustomErrorMessage.java
+++ src/main/java/project/accountservice/exception/CustomErrorMessage.java
@@ -1,18 +1,42 @@
 package project.accountservice.exception;
 
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+
 import java.time.LocalDateTime;
 
+@JsonPropertyOrder({"timestamp", "status", "error", "message", "path"})
 public class CustomErrorMessage {
-    private int statusCode;
     private LocalDateTime timestamp;
+    private int status;
+    private String error;
     private String message;
-    private String description;
+    private String path;
 
-    public CustomErrorMessage(int statusCode,
-                              LocalDateTime timestamp,
-                              String message) {
-        this.statusCode = statusCode;
+    public CustomErrorMessage(LocalDateTime timestamp, int status, String error, String message, String path) {
         this.timestamp = timestamp;
+        this.status = status;
+        this.error = error;
         this.message = message;
+        this.path = path;
+    }
+
+    public LocalDateTime getTimestamp() {
+        return timestamp;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public String getError() {
+        return error;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public String getPath() {
+        return path;
     }
 }
