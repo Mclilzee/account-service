60d8dce9973f62d57091945244c9d0876292d548
Emad Ali <mc.lilzee@hotmail.com>
2022-11-11 03:15:19 +0100

    Add packages for configurations

--- src/main/java/project/accountservice/configurations/SecurityConfiguration.java
+++ src/main/java/project/accountservice/configurations/SecurityConfiguration.java
@@ -1,4 +1,4 @@
-package project.accountservice;
+package project.accountservice.configurations;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
