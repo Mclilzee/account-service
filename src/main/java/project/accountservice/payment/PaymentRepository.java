e9460cbbb0256b7a52f09a4fec7e637a822e7e59
Emad Ali <mc.lilzee@hotmail.com>
2022-11-12 04:53:13 +0100

    Add finding payment by two columns

--- src/main/java/project/accountservice/payment/PaymentRepository.java
+++ src/main/java/project/accountservice/payment/PaymentRepository.java
@@ -3,4 +3,5 @@ package project.accountservice.payment;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PaymentRepository extends JpaRepository<Payment, Long> {
+    Payment findByEmployeeAndPeriod(String email, String period);
 }
