b09727760a6784c35410d7a943c90f3524dfad39
Emad Ali <mc.lilzee@hotmail.com>
2022-11-12 02:54:17 +0100

    Add validation

--- src/main/java/project/accountservice/payment/PaymentController.java
+++ src/main/java/project/accountservice/payment/PaymentController.java
@@ -2,20 +2,25 @@ package project.accountservice.payment;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+@Validated
 @RestController
 public class PaymentController {
 
     @PostMapping("/api/acct/payments")
-    public ResponseEntity<Object> addPayments(@Valid @RequestBody List<Payment> payments) {
+    public ResponseEntity<Map<String, String>> addPayments(@RequestBody List<@Valid Payment> payments) {
 
-        return new ResponseEntity<>(payments, HttpStatus.OK);
+        Map<String, String> response = new HashMap<>();
+        response.put("status", "Added successfully!");
+        return new ResponseEntity<>(response, HttpStatus.OK);
     }
 }
