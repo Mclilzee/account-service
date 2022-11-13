c6d5cb344b745d1b7365537acd7e29f70b44de07
Emad Ali <mc.lilzee@hotmail.com>
2022-11-13 21:10:58 +0100

    Add methods to repository

--- src/main/java/project/accountservice/payment/PaymentRepository.java
+++ src/main/java/project/accountservice/payment/PaymentRepository.java
@@ -3,8 +3,12 @@ package project.accountservice.payment;
 import org.springframework.data.jpa.repository.JpaRepository;
 import project.accountservice.user.User;
 
+import java.util.List;
+
 public interface PaymentRepository extends JpaRepository<Payment, Long> {
     boolean existsByEmployeeAndPeriod(User employee, String period);
 
     Payment findByEmployeeAndPeriod(User employee, String period);
+
+    List<Payment> findAllByEmployee(User employee);
 }
