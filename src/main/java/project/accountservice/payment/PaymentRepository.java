b5c3d922f527ddf7e214e364baa92359033a9c3f
Mclilzee <mc.lilzee@hotmail.com>
2022-11-13 19:10:13 +0100

    Remove unused class

--- src/main/java/project/accountservice/payment/PaymentRepository.java
+++
@@ -1,8 +0,0 @@
-package project.accountservice.payment;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.List;
-
-public interface PaymentRepository extends JpaRepository<Payment, Long> {
-}
