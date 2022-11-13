c400641c486dff15ce9577780a55d94005424dd5
Emad Ali <mc.lilzee@hotmail.com>
2022-11-13 17:05:00 +0100

    Remove unused methods

--- src/main/java/project/accountservice/user/UserRepository.java
+++ src/main/java/project/accountservice/user/UserRepository.java
@@ -2,6 +2,8 @@ package project.accountservice.user;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface UserRepository extends JpaRepository<User, Long> {
     User findByEmail(String email);
 
