b064a1158bb43847da0f0c4f49129156bc6707d9
Emad Ali <mc.lilzee@hotmail.com>
2022-11-13 15:49:27 +0100

    Add PaymentRequest json format class

---
+++ src/main/java/project/accountservice/payment/PaymentRequest.java
@@ -0,0 +1,48 @@
+package project.accountservice.payment;
+
+import javax.validation.constraints.Min;
+import javax.validation.constraints.Pattern;
+
+public class PaymentRequest {
+
+    private String employee;
+
+    @Pattern(regexp = "(0[1-9]|1[0-2])-20\\d{2}", message = "Incorrect period format")
+    private String period;
+
+    @Min(value = 0, message = "Salary cannot be negative")
+    private long salary;
+
+    public PaymentRequest(String employee, String period, long salary) {
+        this.employee = employee;
+        this.period = period;
+        this.salary = salary;
+    }
+
+    public PaymentRequest() {
+    }
+
+    public String getEmployee() {
+        return employee;
+    }
+
+    public void setEmployee(String employee) {
+        this.employee = employee;
+    }
+
+    public String getPeriod() {
+        return period;
+    }
+
+    public void setPeriod(String period) {
+        this.period = period;
+    }
+
+    public long getSalary() {
+        return salary;
+    }
+
+    public void setSalary(long salary) {
+        this.salary = salary;
+    }
+}
