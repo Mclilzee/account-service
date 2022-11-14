793390fa984aaa1f98caac82ef836594b0b22ff7
Emad Ali <mc.lilzee@hotmail.com>
2022-11-14 20:23:13 +0100

    Refactor

--- src/main/java/project/accountservice/user/User.java
+++ src/main/java/project/accountservice/user/User.java
@@ -8,7 +8,6 @@ import javax.persistence.*;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
-import java.nio.channels.WritableByteChannel;
 import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
