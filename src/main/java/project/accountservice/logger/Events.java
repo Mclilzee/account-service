04deea84651e490abce51d88c608292c4d8bfa0d
Emad Ali <mc.lilzee@hotmail.com>
2022-11-15 00:11:26 +0100

    Add event actions

--- src/main/java/project/accountservice/logger/Events.java
+++ src/main/java/project/accountservice/logger/Events.java
@@ -1,5 +1,16 @@
 package project.accountservice.logger;
 
+import org.springframework.web.bind.annotation.CookieValue;
+
 public enum Events {
-    CREATE_USER
+    CREATE_USER,
+     CHANGE_PASSWORD,
+    ACCESS_DENIED,
+    LOGIN_FAILED,
+    GRANT_ROLE,
+    REMOVE_ROLE,
+    LOCK_USER,
+    UNLOCK_USER,
+    DELETE_USER,
+    BRUTE_FORCE
 }
