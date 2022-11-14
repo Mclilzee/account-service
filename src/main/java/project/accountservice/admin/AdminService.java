7cdbef24d7f7ba5835287be933060d006ccc5ff3
Emad Ali <mc.lilzee@hotmail.com>
2022-11-14 02:06:31 +0100

    Adjust service to not be able to delete Adminstrators

--- src/main/java/project/accountservice/admin/AdminService.java
+++ src/main/java/project/accountservice/admin/AdminService.java
@@ -1,7 +1,10 @@
 package project.accountservice.admin;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+import project.accountservice.user.Role;
 import project.accountservice.user.User;
 import project.accountservice.user.UserRepository;
 
@@ -15,5 +18,15 @@ public class AdminService {
 
     public List<User> getUsers() {
         return userRepository.findAll();
+    }
+
+    public void deleteUser(String email) {
+        User user = userRepository.findByEmail(email);
+        if (user == null) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "User not found!");
+        } else if (user.getRoles().contains(Role.getAdministrator().getRole())) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Can't remove ADMINISTRATOR role!");
+        }
+        userRepository.delete(user);
     }
 }
