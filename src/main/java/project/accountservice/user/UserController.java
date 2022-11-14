81088d2e89b1242424973f3e9f4865abe80e7c44
Emad Ali <mc.lilzee@hotmail.com>
2022-11-14 01:12:50 +0100

    Saparate controller and service logic

--- src/main/java/project/accountservice/user/UserController.java
+++ src/main/java/project/accountservice/user/UserController.java
@@ -1,15 +1,11 @@
 package project.accountservice.user;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.server.ResponseStatusException;
-import project.accountservice.util.PasswordUtil;
 
 import javax.validation.Valid;
 import java.util.LinkedHashMap;
@@ -19,34 +15,18 @@ import java.util.Map;
 public class UserController {
 
     @Autowired
-    BCryptPasswordEncoder encoder;
-
-    @Autowired
-    UserRepository userRepository;
+    UserService userService;
 
     @PostMapping("/api/auth/signup")
     public User signUp(@Valid @RequestBody User user) {
-        if (PasswordUtil.isBreached(user.getPassword())) {
-            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "The password is in the hacker's database!");
-        }
-        if (userRepository.existsByEmail(user.getEmail())) {
-            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "User exist!");
-        }
-
-        user.setPassword(encoder.encode(user.getPassword()));
-        return userRepository.save(user);
+        return userService.addUser(user);
     }
 
     @PostMapping("/api/auth/changepass")
     public Map<String, String> changePassword(
             @AuthenticationPrincipal UserDetails userDetails,
             @Valid @RequestBody Password newPassword) {
-
-        if (PasswordUtil.isBreached(newPassword.getPassword())) {
-            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "The password is in the hacker's database!");
-        }
-
-        saveUserToDatabase(userDetails, newPassword);
+        userService.changeUserPassword(userDetails, newPassword);
         return getPasswordChangeSuccessResponse(userDetails);
     }
 
@@ -55,16 +35,5 @@ public class UserController {
         responseBody.put("email", userDetails.getUsername());
         responseBody.put("status", "The password has been updated successfully");
         return responseBody;
-    }
-
-    private void saveUserToDatabase(UserDetails userDetails, Password newPassword) {
-        User user = userRepository.findByEmail(userDetails.getUsername());
-
-        if (encoder.matches(newPassword.getPassword(), user.getPassword())) {
-            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "The passwords must be different!");
-        }
-
-        user.setPassword(encoder.encode(newPassword.getPassword()));
-        userRepository.save(user);
     }
 }
