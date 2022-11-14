2a6bcdde9b65d8a4f04f958fba24dc227f54fe9c
Emad Ali <mc.lilzee@hotmail.com>
2022-11-14 01:13:45 +0100

    Refactor

--- src/main/java/project/accountservice/user/UserService.java
+++ src/main/java/project/accountservice/user/UserService.java
@@ -2,12 +2,13 @@ package project.accountservice.user;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 import project.accountservice.util.PasswordUtil;
 
-import java.util.List;
+import java.util.Set;
 
 @Service
 public class UserService {
@@ -19,26 +20,46 @@ public class UserService {
     UserRepository userRepository;
 
     public User addUser(User user) {
-        validateUser(user);
+        checkIfPasswordBreached(user.getPassword());
+        checkIfUserAlreadyExist(user);
         setRole(user);
         user.setPassword(encoder.encode(user.getPassword()));
         return userRepository.save(user);
     }
 
+    public void changeUserPassword(UserDetails userDetails, Password newPassword) {
+        checkIfPasswordBreached(newPassword.getPassword());
+
+        User user = userRepository.findByEmail(userDetails.getUsername());
+        checkIfSamePassword(newPassword, user);
+
+        user.setPassword(encoder.encode(newPassword.getPassword()));
+        userRepository.save(user);
+    }
+
     private void setRole(User user) {
         if (userRepository.count() == 0){
-            user.setRoles(List.of(Roles.ROLE_ADMINISTRATOR));
+            user.setRoles(Set.of(Role.getAdministrator()));
         } else {
-            user.setRoles(List.of(Roles.ROLE_USER));
+            user.setRoles(Set.of(Role.getUser()));
         }
     }
 
-    private void validateUser(User user) {
-        if (PasswordUtil.isBreached(user.getPassword())) {
-            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "The password is in the hacker's database!");
-        }
+    private void checkIfUserAlreadyExist(User user) {
         if (userRepository.existsByEmail(user.getEmail())) {
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "User exist!");
+        }
+    }
+
+    private void checkIfSamePassword(Password newPassword, User user) {
+        if (encoder.matches(newPassword.getPassword(), user.getPassword())) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "The passwords must be different!");
+        }
+    }
+
+    private void checkIfPasswordBreached(String password) {
+        if (PasswordUtil.isBreached(password)) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "The password is in the hacker's database!");
         }
     }
 }
